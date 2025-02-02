60ef05ebb0a52c0fe0c8e789b0da2cdc433f1085
Kyubin1123 <michelle010929@gmail.com>
2025-02-03 05:23:02 +0900

    feat: swagger test

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from django.urls import re_path
+from django.urls import include, path, re_path
+from django.conf import settings
+from rest_framework import routers
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+router = routers.DefaultRouter()
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,9 +35,14 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny,],
 )
 
-urlpatterns = [
+urlpatterns=[
     path("admin/", admin.site.urls),
-    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("api/", include(('webtoons.urls', 'api'))),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<formet>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
