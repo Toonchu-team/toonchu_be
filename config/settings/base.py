18cc1cd1b80dbd8b7dd7de0fa45161a01927cd9f
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2025-02-03 15:26:28 +0900

    env복구 but 되던기능 작동x

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,7 @@ from dotenv import dotenv_values
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# env 파일 로드
+# .env 파일 로드
 ENV = dotenv_values(".env")
 
 # Quick-start development settings - unsuitable for production
