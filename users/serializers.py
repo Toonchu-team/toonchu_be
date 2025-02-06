453b9ca8ce93009f178f4b5ee09f8ebc159b9d00
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2025-02-06 16:34:51 +0900

    user model 수정

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+# from rest_framework import serializers
+#
+# from users.models import CustomUser
+#
+#
+# class LogoutSerializer(serializers.Serializer):
+#     refresh_token = serializers.CharField(required=True)
+#
+# class UserProfileUpdateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['nick_name', 'profile_img']
+#         extra_kwargs = {
+#             'nick_name': {'required': False},
+#             'profile_img': {'required': False},
+#         }
