82d549b098e5965374be2282cb953f89633cd051
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2025-02-12 19:06:21 +0900

    소셜로그인

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,3 +11,64 @@ ALLOWED_HOSTS = [
 
 
 CORS_ALLOW_ALL_ORIGINS = True  # 개발 환경에서만 사용
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'console.info': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'console.error': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'logger.info': {
+            'level': 'INFO',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'logger.warning': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'logger.error': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    }
+}
--- users/urls.py
+++ users/urls.py
@@ -1,28 +1,32 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import (
-    GoogleCallbackView,
-    GoogleLoginView,
-    KakaoCallbackView,
-    KakaoLoginView,
-    LogoutView,
-    NaverCallbackView,
-    NaverLoginView,
-    UserProfileView,
-    UserWithdrawView,
-)
+    # GoogleCallbackView,
+    # GoogleLoginView,
+    # KakaoCallbackView,
+    # KakaoLoginView,
+    # LogoutView,
+    # NaverCallbackView,
+    # NaverLoginView,
+    # UserProfileView,
+    # UserWithdrawView,
+    SocialLoginView)
 
 urlpatterns = [
-    # Social Login URLs
-    path("login/kakao/", KakaoLoginView.as_view(), name="kakao-login"),
-    path("login/google/", GoogleLoginView.as_view(), name="google-login"),
-    path("login/naver/", NaverLoginView.as_view(), name="naver-login"),
-    # Social Callback URLs - GET 메소드 제거
-    path("callback/google/", GoogleCallbackView.as_view(), name="google-callback"),
-    path("callback/kakao/", KakaoCallbackView.as_view(), name="kakao-callback"),
-    path("callback/naver/", NaverCallbackView.as_view(), name="naver-callback"),
+    # # Social Login URLs
+    # path("login/kakao/", KakaoLoginView.as_view(), name="kakao-login"),
+    # path("login/google/", GoogleLoginView.as_view(), name="google-login"),
+    # path("login/naver/", NaverLoginView.as_view(), name="naver-login"),
+    # # Social Callback URLs - GET 메소드 제거
+    # path("callback/google/", GoogleCallbackView.as_view(), name="google-callback"),
+    # path("callback/kakao/", KakaoCallbackView.as_view(), name="kakao-callback"),
+    # path("callback/naver/", NaverCallbackView.as_view(), name="naver-callback"),
     # User Management URLs
+
+    path('login/<str:provider>/', SocialLoginView.as_view(), name='social_login'),
+
     path("me/logout/", LogoutView.as_view(), name="logout"),
-    path("me/profile/update/", UserProfileView.as_view(), name="profile-update"),
-    path("me/profile/withdraw/", UserWithdrawView.as_view(), name="profile-withdraw"),
+    # path("me/profile/update/", UserProfileView.as_view(), name="profile-update"),
+    # path("me/profile/withdraw/", UserWithdrawView.as_view(), name="profile-withdraw"),
 ]
