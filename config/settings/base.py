385a5e929c4e091ad4e201709d2419a832e46c29
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2025-02-26 12:51:45 +0900

    ..

--- config/settings/base.py
+++ config/settings/base.py
@@ -233,7 +233,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     # "http://localhost:3000",
     # "https://toonchu-fe.vercel.app",
-    "https://toonchu.com"
+    "https://toonchu.com",
 ]
 CORS_ALLOW_CREDENTIALS = True  # 인증정보 포함 허용
 
